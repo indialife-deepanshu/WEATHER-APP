16d8c23ca1378eb3b005c560834cfe4c1f4ef751
allindiacoderlife <chetanbharangar8938@gmail.com>
2025-12-18 13:39:20 +0530

    fix: Correct filename casing for InfoBox.css import

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './InfoBox.css';
+import './Infobox.css';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
